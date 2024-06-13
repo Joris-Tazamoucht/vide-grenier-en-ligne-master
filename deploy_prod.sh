45ba291e3f01c2c6ca06cd47851a37176e2ad907
joris13130 <joris.tazamoucht@gmail.com>
2024-06-13 10:51:47 +0200

    hotfix

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Récupérer les derniers changements de la branche master depuis le dépôt Git
-git pull origin master
+git pull origin dev
 
 # Arrêter et supprimer les conteneurs existants pour l'environnement de production
 docker-compose -f docker-compose-prod.yml -p prod down
