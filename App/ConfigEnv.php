69ade779b055fd4fed0389d3892d7f36235ef976
joris13130 <joris.tazamoucht@gmail.com>
2024-07-10 11:11:48 +0200

    hotfix

--- App/ConfigEnv.php
+++ App/ConfigEnv.php
@@ -3,15 +3,22 @@
 namespace App;
 
 use \App\Helpers\DotEnv;
+
 /**
  * Application configuration
  *
  * PHP version 7.0
  */
 
+// Assurez-vous que ROOT est défini avant d'utiliser DotEnv
+if (!defined('ROOT')) {
+    define('ROOT', '/var/www/html/');  // Assurez-vous que ce chemin est correct
+}
+
+// Chargez les variables d'environnement depuis le fichier .env
 (new DotEnv(ROOT . '.env'))->load();
-return
-[
+
+return [
     //
     // Cookie Config
     // =========================================================================
@@ -21,9 +28,12 @@ return
     // Core Config
     // =========================================================================
     "DB_HOST" => getenv('DB_HOST'),
-    "DB_PORT" => getenv('TARGET_DB_PORT'),
+    "DB_PORT" => getenv('DB_PORT'),  // Corrigé pour correspondre à la variable d'environnement
     "DB_NAME" => getenv('DB_NAME'),
     "DB_USER" => getenv('DB_USER'),
     "DB_PASSWORD" => getenv('DB_PASSWORD'),
+    "SMTP_MAIL_ADRESS" => getenv('SMTP_MAIL_ADRESS'),
+    "SMTP_MAIL_PASSWORD" => getenv('SMTP_MAIL_PASSWORD'),
+    "SMTP_SERVER" => getenv('SMTP_SERVER'),
     "SHOW_ERRORS" => true,
 ];
